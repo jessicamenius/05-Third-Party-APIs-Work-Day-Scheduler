a224f3c8e2c3755fe719aee9d9155e99bbf6bb35
Jessica <jessicamenius@gmail.com>
2020-04-30 00:44:08 -0500

    added date

---
+++ app.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  var currentDay = moment().format("dddd, MMMM DD, YYYY");
+  console.log(moment().format("dddd, MMMM DD, YYYY"));
+  $("#currentDay").text(currentDay);
+});
