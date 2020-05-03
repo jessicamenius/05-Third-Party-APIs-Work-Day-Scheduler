7ae5be682747001e2559cf24818254ac4097d12c
Jessica <jessicamenius@gmail.com>
2020-05-03 00:41:57 -0500

    making sure updates are there

--- README.md
+++ README.md
@@ -19,7 +19,6 @@ GIVEN I am using a daily planner to create a schedule
 
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
-
     *Use moment to grab current day of week, month day, year*
 
 WHEN I scroll down
@@ -28,20 +27,22 @@ THEN I am presented with timeblocks for standard business hours
 
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-For loop
+    *If/else statement  - if now < start of hour - red, elseif now = start of hour - red, else now > start of hour - green  *
 
 WHEN I click into a timeblock
 THEN I can enter an event
     *Create a textarea, use event.preventDefault():*
+    *If/else statement - if now < start of hour, no comments can be added, else now >start of hour, comments can be added.
 
 WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
-    *Use event.preventDefault():* Save button, add to local storage,#
+    *Use event.preventDefault():* Save button, add time/meeting info to  local storage.#
 
 
 WHEN I refresh the page
 THEN the saved events persist
     *Events added to local storage*
+    *Events don't leave page
 ```
 
 The following animation demonstrates the application functionality:
--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 $(document).ready(function () {
   // WHEN I open the planner, THEN the current day is displayed at the top of the calendar
   $("#currentDay").text(moment().format("dddd, MMMM DD, YYYY"));
-  $("#saveBtn").on("click", function (e) {
-    e.preventDefault();
-    meetingInfo = $("#meetingInfo").val();
-    console.log(meetingInfo);
-  });
-
-  // var currentTime = moment().moment().startOf('hour');
 });
+
+$(document).ready(function () {
+  $("#saveBtn").on("click", function () {});
+});
+
+//     // Add to local storage
+
+//     var meetingTime = $("#meetingTime").val();
+//     var meetingInfo = $("#meetingInfo").val();
+
+//     console.log(meetingInfo);
+//     console.log(meetingTime);
+
+//     $("#saveBtn").on("click", function (e) {
+//       e.preventDefault();
+
+//     // WHEN I view the timeblocks for that day, THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+//     var currentHour = moment().startOf("hour");
+//     var meetingHour = moment().hour($(this).attr("data-time")).startOf("hour");
+
+//     if (meetingHour < currentHour) {
+//       // make backgroundd grey
+
+//     }elseif(meetingHour > currentHour) {
+//       // make background green
+//     }else{
+//       // make background red
+
+//     }:
+//   });
+// });
